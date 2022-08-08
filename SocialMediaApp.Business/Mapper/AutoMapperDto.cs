90e6b7700c3f1f35d52e95cf6401bcdb3d0538c5
Hasan Akpolad <hsnakpld@gmail.com>
2022-08-08 16:31:40 +0300

    configure AutoMapper

--- SocialMediaApp.Business/Mapper/AutoMapperDto.cs
+++ SocialMediaApp.Business/Mapper/AutoMapperDto.cs
@@ -8,24 +8,9 @@ namespace SocialMediaApp.Business.Mapper
     {
         public AutoMapperDto()
         {
-            CreateMap<UserModel, UserDto>()
-                .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
-                .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
-                .ForMember(x => x.Gender, y => y.MapFrom(z => z.Gender))
-                .ForMember(x => x.BirthDate, y => y.MapFrom(z => z.BirthDate))
-                .ForMember(x => x.Role, y => y.MapFrom(z => z.Role))
-                .ForMember(x => x.FollowCount, y => y.MapFrom(z => z.FollowCount))
-                .ForMember(x => x.FollowerCount, y => y.MapFrom(z => z.FollowerCount))
-                .ForMember(x => x.Addresses, y => y.MapFrom(z => z.Addresses));
+            CreateMap<UserModel, UserDto>();
 
-            CreateMap<UserDto, UserModel>()
-                .ForMember(x => x.Name, y => y.MapFrom(z => z.Name))
-                .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
-                .ForMember(x => x.Gender, y => y.MapFrom(z => z.Gender))
-                .ForMember(x => x.BirthDate, y => y.MapFrom(z => z.BirthDate))
-                .ForMember(x => x.Role, y => y.MapFrom(z => z.Role))
-                .ForMember(x => x.FollowCount, y => y.MapFrom(z => z.FollowCount))
-                .ForMember(x => x.FollowerCount, y => y.MapFrom(z => z.FollowerCount));
+            CreateMap<UserDto, UserModel>();
 
             CreateMap<AddressModel, AddressDto>()
                 .ForMember(x => x.Owner, y => y.MapFrom(z => z.User.Name + " " + z.User.LastName));
